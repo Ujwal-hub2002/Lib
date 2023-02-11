2f4aea74d935a0ad263a74e4f0533b0178fb7516
Ujwal-hub2002 <79373107+Ujwal-hub2002@users.noreply.github.com>
2023-02-11 13:31:14 +0530

    Add files via upload

---
+++ src/DataTable.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {AiFillDelete} from "react-icons/ai"
+const DataTable = ({data}) => {
+    if(data.length===0) 
+    return <div className='bg-white py-2 px-2'>No Books are added yet</div>
+    
+  return (
+    <div className='table'>
+    <table >
+      <thead>
+        <tr className='tr'>
+          <th className='td'>ISBN#</th>
+          <th className='td'>Title</th>
+          <th className='td'>Author</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((data, index) => (
+          <tr key={index} className="tr">
+            <td>{data.title}</td>
+            <td>{data.author}</td>
+            <td>{data.isbn}</td>
+            <td ><AiFillDelete/></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <button className='remove-btn'>Remove All</button>
+    </div>
+  )
+}
+
+export default DataTable
